dd2151f60c67020ff295fc45649e01f1c54c249e
raylu15742cs <raylucs15742@gmail.com>
2023-08-11 14:39:46 -0700

    update

--- utils/inputs/pdf.py
+++ utils/inputs/pdf.py
@@ -1,10 +1,9 @@
 from pathlib import Path
-
 from PyPDF2 import PdfReader
+from typing import Union
 from typing import IO
 
-
-def parse_pdfs(pdf_docs: str | IO | Path):
+def parse_pdfs(pdf_docs: Union[str, IO, Path]):
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
