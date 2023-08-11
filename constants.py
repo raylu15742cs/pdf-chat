8c344f04d6293bac0f96220ec4591841054ed162
raylu15742cs <raylucs15742@gmail.com>
2023-08-11 13:21:06 -0700

    update

--- constants.py
+++ constants.py
@@ -3,10 +3,11 @@ from typing import Final
 from os import environ
 
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
-PINECONE_API_KEY: Final[str] = environ["PINECONE_API_KEY"]
+PINECONE_API_KEY: Final[str] = st.secrets["API_KEYS"]["pinecone"]
 PINECONE_API_ENV: Final[str] = "asia-southeast1-gcp-free"
 OPENAI_API_KEY: Final[str] = environ["OPENAI_API_KEY"]
 INDEX_NAME: Final[str] = 'salesforcedocs'
